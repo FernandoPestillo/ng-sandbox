01d6715e345710a1c2cabe44d12a8fee73cafc50
FernandoPestillo <fernandopestillo@gmail.com>
2025-11-24 13:07:17 -0300

    Estilozando Home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit {
+  items: number[] = [];
+  counter = 0;
 
+  ngOnInit(): void {
+    setInterval(() => {
+      this.items.push(this.counter++);
+    }, 10);
+  }
 }
