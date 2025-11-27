8d1141fd06bdfb880b457d6a6cd61e25c621587a
FernandoPestillo <fernandopestillo@gmail.com>
2025-11-27 09:21:43 -0300

    Instalando EJS-Grid

--- src/app/components/table/table.ts
+++ src/app/components/table/table.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GridModule } from '@syncfusion/ej2-angular-grids';
+import { data } from '../../data/datasource';
 
 @Component({
   selector: 'app-table',
-  imports: [],
+  imports: [GridModule],
   templateUrl: './table.html',
   styleUrl: './table.scss',
 })
-export class Table {
+export class Table implements OnInit {
+  public data?: object[];
 
+  ngOnInit(): void {
+    this.data = data;
+  }
 }
