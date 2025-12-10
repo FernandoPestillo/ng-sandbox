09fe098b7a5f0c31242fb209c84b31e24a4dee3c
FernandoPestillo <fernandopestillo@gmail.com>
2025-12-10 12:25:37 -0300

    Registrando licenças

--- src/app/components/stimulsoft-designer/stimulsoft-designer.ts
+++ src/app/components/stimulsoft-designer/stimulsoft-designer.ts
@@ -13,6 +13,9 @@ export class StimulsoftDesigner implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
+    Stimulsoft.Base.StiLicense.loadFromString(
+      '6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHkRAoWOWENBBUYxsAJlAX73U4zQw79rappzLAF7SbgOX4WRdSc1vTH/YWoHigo0uhX9mrx7u2FhFxXycBsTjgFw0GnHhXzIglVfu8Efb7t/TtFDOTVag3Dtj3rLWRIX8sx8EAMB2TUz6rHspw8VRnl58dh7e/bj5LS/7AmoxwryGHO1m3gc2FkWfHfAN4qKlHOAPN6EJbXafjmVqaVv2jypV+l/ZBX4q3QHu81XVDSvfz7XFibamc5tYa2HjK1Yq0g6gymYvU2wD3tj6x63FXcIloXEuJqjyx2lRF9PyQzzHhzWH0vZeExBjBK77dDKQwUqrmFthcUQXkF7LHSlzaSPpyERuOlZ5bb1rdjTmuj07YmN/SWBT2Yz186+Xx4TYKpUlXqRl5PhDlfnq0tGlwCpypUX2kH25keHvFJCIsmeNUJWpM/OKBqA6BNR6jmZqn639o9miIX9MOl56BP9TUhkTe5M12d4ZP7pUkj31acAPYh85HAOpmXqqUmBlI0rb+5yPMvq6z75A7bGm4JmHJuh'
+    );
     // var report = new Stimulsoft.Report.StiReport();
     // report.loadFile('reports/Invoice.mrt');
 
