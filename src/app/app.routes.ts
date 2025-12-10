59c5ec61ddc95f1c218fcb1ee7354950674cfdc4
FernandoPestillo <fernandopestillo@gmail.com>
2025-12-10 14:12:50 -0300

    Criando Dialog component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Home } from './pages/home/home';
 import { Table } from './components/table/table';
 import { StimulsoftReport } from './components/stimulsoft-report/stimulsoft-report';
 import { StimulsoftDesigner } from './components/stimulsoft-designer/stimulsoft-designer';
+import { Dialog } from './components/dialog/dialog';
 
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: 'dialog', component: Dialog },
   { path: 'table', component: Table },
   { path: 'report', component: StimulsoftReport },
   { path: 'report-designer', component: StimulsoftDesigner },
