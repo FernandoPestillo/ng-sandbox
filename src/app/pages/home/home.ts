2b02106a9707727ef2e7b0f6cf5884bc102dd2e1
FernandoPestillo <fernandopestillo@gmail.com>
2025-12-10 13:12:47 -0300

    Instalando Toastr

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +11,16 @@ export class Home implements OnInit {
   items: number[] = [];
   counter = 0;
 
+  constructor(private toastr: ToastrService) {}
+
   ngOnInit(): void {
     while (this.counter < 999) {
       this.items.push(this.counter++);
     }
+  }
+
+  exibirAlerta() {
+    console.log('TOASTR');
+    this.toastr.warning('TOASTR');
   }
 }
