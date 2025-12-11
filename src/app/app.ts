4571c7944f58b69064c538f2cddc5d06054e4159
FernandoPestillo <fernandopestillo@gmail.com>
2025-12-11 14:22:11 -0300

    RouterLink ao invés de href

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,15 +3,21 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { Theme } from './services/configurations/theme';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ModalManagerComponent } from './components/modal-manager-component/modal-manager-component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, MatDialogModule, ModalManagerComponent],
+  imports: [CommonModule, RouterOutlet, RouterModule, MatDialogModule, ModalManagerComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('ng-sandbox');
+
+  navLinks = [
+    { link: '/', text: 'Home' },
+    { link: '/test-page', text: 'Test Page' },
+  ];
 
   constructor(private themeService: Theme) {}
 
@@ -21,5 +27,13 @@ export class App {
 
   get currentTheme() {
     return this.themeService.getCurrentTheme();
+  }
+
+  protected menuToggle() {
+    const mobileMenu = document.getElementById('mobileMenu');
+
+    if (mobileMenu) {
+      mobileMenu.classList.toggle('hidden');
+    }
   }
 }
